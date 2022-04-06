6ff7166e6cf674745ba14d804939ad284a5e897d
Matheus Cardoso <matheus@cardo.so>
2022-04-06 12:28:11 -0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -9,9 +9,6 @@ import ApolloClient from 'apollo-boost';
 
 const client = new ApolloClient({
   uri: 'https://api.github.com/graphql',
-  headers: {
-    authorization: `Bearer d81785767ecd76ada5136de8139208c753dcf216`
-  }
 });
 
 ReactDOM.render(
